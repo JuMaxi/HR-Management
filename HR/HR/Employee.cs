acb30efc539b6b100343c9a2ad6bb1a594db19e5
JuMaxi <jmaximovski@gmail.com>
2022-12-28 17:31:22 +0000

    Creating Employee Class

---
+++ HR/HR/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR
+{
+    internal class Employee
+    {
+        public string Name;
+        public string CPF;
+        public string Registry;
+        public int DateStart;
+        public double MonthlySalary;
+
+        public void Initialize(string NameEmployee, string NumberCPF, string NumberRegistry, int Date, double Salary)
+        {
+            Name = NameEmployee;
+            CPF = NumberCPF;
+            Registry = NumberRegistry;
+            DateStart = Date;
+            MonthlySalary = Salary;
+        }
+
+        public double CalculateSalary()
+        {
+            MonthlySalary = (MonthlySalary / 30) * (30 - (DateStart - 1));
+            double INSS = MonthlySalary * 0.07;
+            double IR = (MonthlySalary - INSS) * 0.15;
+
+            double LiquidSalary = MonthlySalary - INSS - IR;
+
+            Console.WriteLine("Hello, " + Name + ", Number Registry " + Registry + " follow below your salary details: ");
+            Console.WriteLine("Days worked this month: " + (30 - (DateStart - 1)));
+            Console.WriteLine("Monthly Salary(proportional days worked): £ " + MonthlySalary);
+            Console.WriteLine("INSS: £ (" + INSS + ")");
+            Console.WriteLine("IRRF: £ (" + IR + ")");
+            Console.WriteLine("Liquid Salary: £ " + LiquidSalary);
+            Console.WriteLine(" ");
+
+            return LiquidSalary;
+        }
+        
+    }
+}
--- HR/HR/Program.cs
+++ HR/HR/Program.cs
@@ -6,7 +6,18 @@ namespace HR
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee Employee1 = new Employee();
+            Employee1.Initialize("Joao da Silva", "123.456.789-10", "45.125", 01, 3000);
+            
+            Employee Employee2 = new Employee();
+            Employee2.Initialize("Joao Santos", "234.569.458-23", "45.121", 10, 1800);
+
+            Employee Employee3 = new Employee();
+            Employee3.Initialize("Maria Lima", "564.425.569-40", "45.123", 12, 2500);
+
+            Employee1.CalculateSalary();
+            Employee2.CalculateSalary();
+            Employee3.CalculateSalary();
         }
     }
 }
